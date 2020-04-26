0fc3fbe2e676f56b35646b254871acf9973daf97
Tim Acker <tjacker31@gmail.com>
2020-04-26 16:02:04 -0400

    Implements local storage functionanlity

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,21 @@
     const inputElement = document.getElementById(input);
     const root = document.documentElement;
 
+    // Set css variable from local storage if available
+    if (localStorage.getItem(input)) {
+      const storageValue = localStorage.getItem(input);
+
+      root.style.setProperty(cssVariable, storageValue);
+      inputElement.value = storageValue;
+    }
+
+    // Add event listener to change css variable and save to local storage
     inputElement.addEventListener("input", function () {
       root.style.setProperty(cssVariable, this.value);
+      localStorage.setItem(input, this.value);
     });
   };
 
   handleInputEvent("opacity", "--overlay-opacity");
   handleInputEvent("color", "--overlay-color");
 })();
-
-// TODO: Save input preferences to local storage
